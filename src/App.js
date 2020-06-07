998eab06b579633f20b36f0705b3dd7f6c62a2a3
pjehan <pierre.jehan@gmail.com>
2020-06-07 11:52:18 +0200

    Install Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import HomePage from "./components/pages/home/HomePage";
@@ -7,16 +11,23 @@ import AddShortcutPage from "./components/pages/add-shortcut/AddShortcutPage";
 import 'bootstrap';
 import './App.scss';
 
+const store = createStore(
+  null, // TODO: call reducer
+  composeWithDevTools(applyMiddleware(thunkMiddleware))
+);
+
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar/>
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/softwares' component={SoftwaresPage}/>
-        <Route path='/add-shortcut' component={AddShortcutPage}/>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/softwares' component={SoftwaresPage}/>
+          <Route path='/add-shortcut' component={AddShortcutPage}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
