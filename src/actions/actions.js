6da0abc3c0bb5bf2aec5671ac2a0b3251cbbff61
pjehan <pierre.jehan@gmail.com>
2020-06-08 00:22:25 +0200

    Redux Actions fetch shortcuts

---
+++ src/actions/actions.js
@@ -0,0 +1,25 @@
+export const FETCH_SHORTCUTS_PENDING = 'FETCH_SHORTCUTS_PENDING';
+export const FETCH_SHORTCUTS_FAILURE = 'FETCH_SHORTCUTS_FAILURE';
+export const FETCH_SHORTCUTS_SUCCESS = 'FETCH_SHORTCUTS_SUCCESS';
+
+export function fetchShortcuts() {
+  return dispatch => {
+    dispatch(fetchShortcutsPending());
+    fetch('http://127.0.0.1:8000/api/shortcuts')
+      .then(response => response.json())
+      .then(data => dispatch(fetchShortcutsSuccess(data)))
+      .catch(error => dispatch(fetchShortcutsFailure(error)));
+  };
+}
+
+export function fetchShortcutsPending() {
+  return { type: FETCH_SHORTCUTS_PENDING, payload: null };
+}
+
+export function fetchShortcutsFailure(error) {
+  return { type: FETCH_SHORTCUTS_FAILURE, payload: { error: error } };
+}
+
+export function fetchShortcutsSuccess(shortcuts) {
+  return { type: FETCH_SHORTCUTS_SUCCESS, payload: { shortcuts: shortcuts } };
+}
